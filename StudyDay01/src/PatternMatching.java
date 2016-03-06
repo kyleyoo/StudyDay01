f6168f4e22eb878683827ce924d81cf0f683dc56
User <User@Surface2>
2016-03-06 17:50:17 +0900

    스튜던트 객체 생성

--- StudyDay01/src/PatternMatching.java
+++ StudyDay01/src/PatternMatching.java
@@ -1,8 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class PatternMatching {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+//		기존 학생들의 점수와 진학기록의 통계를 내고 경향을 분석하여
+//		내 점수를 가지고 내가 문과를 가야될지 이과를 가야될지 판단해주는 프로그램을 
+//		작성해라.
+	//
+//		여기에서 조건이 주어짐.
+//		데이터를 하나의 클래스로 설계해서 프로그램을 작성하시오.
+		
+		//점수 입력(점수와 문과 이과인지 데이터를 받아야함)
+		Scanner in = new Scanner(System.in);
+		
+		List<Student> scoreresult = new ArrayList<>();
+		//리스트로 점수데이터 입력
+		//식
+		scoreresult.add(new Student(11.1,12.1,13.0,"이"));
+		
+		//내점수를 입력해서 결과 보기
+		//점수를 분석하고
+		//결과 보기
 
 	}
 
--- StudyDay01/src/Student.java
+++ StudyDay01/src/Student.java
@@ -1,4 +1,20 @@
+import java.util.Scanner;
 
 public class Student {
 
+	private double kor;
+	private double eng;
+	private double math;
+	private String code;
+	
+	public Student(double kor, double eng, double math, String code){
+		
+		this.kor = kor;
+		this.eng = eng;
+		this.math = math;
+		this.code = code;
+		
+	}
+
+	
 }
